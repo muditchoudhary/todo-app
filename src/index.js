2998c0353cc175b884da8835b9ec9c4024c88f18
Mudit Choudhary <cmudit60@gmail.com>
2022-07-21 19:35:40 +0530

    Add listener to add button inside form

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import { myListener as Listener } from "./modules/listener";
 Listener.listenClickOnElements(".add-task", () => {Dom.openForm()});
 Listener.listenClickOnElements(".close", () => {Dom.closeForm()});
 Listener.listenClickOnElements(".cancel", () => {Dom.closeForm()});
+Listener.listenClickOnElements(".confirm-add-task-btn", () => {Dom.createNewTask()});
 
 console.log("working");
