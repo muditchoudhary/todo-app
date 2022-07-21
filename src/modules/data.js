0a15cf8b10ee4bcb4259b1cf105ed7a3fd7e4be9
Mudit Choudhary <cmudit60@gmail.com>
2022-07-21 19:36:41 +0530

    created data.js to get data values

---
+++ src/modules/data.js
@@ -0,0 +1,17 @@
+const Data = () => {
+	const getDatafromForm = () => {
+		const title = document.querySelector("#title").value;
+		const dueDate = document.querySelector("#due-date").value;
+		const description = document.querySelector("#description").value;
+		const priority = document.querySelector("#priority").value;
+		const project = document.querySelector("#project").value;
+
+		return { title, dueDate, description, priority, priority, project };
+	};
+
+	return { getDatafromForm };
+};
+
+const myData = Data();
+
+export { myData };
