316fcfe2fc85fe8bac0ce6822a47c307b381c66b
Mudit Choudhary <cmudit60@gmail.com>
2022-07-29 14:03:37 +0530

    moved save data locally method codes to data.js

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,5 +1,8 @@
 const Data = () => {
 	const getDatafromForm = () => {
+        /**
+         * Fetch out all the data values from the form fields
+         */
 		const title = document.querySelector("#title").value;
 		const dueDate = document.querySelector("#due-date").value;
 		const description = document.querySelector("#description").value;
@@ -9,7 +12,59 @@ const Data = () => {
 		return { title, dueDate, description, priority, priority, project };
 	};
 
-	return { getDatafromForm};
+    const getLocalTasksData = (key) => {
+        /** 
+         * This gets the data from localStorage of a given key
+         * if no key is there return an empty object
+        */
+       return localStorage.getItem(key) ? localStorage.getItem(key) : {};
+    }
+
+    const setLocalTaskData = (key, data) => {
+        /**
+         * This sets the data to localStorage of a given key
+         */
+        localStorage.setItem(key, data);
+        
+    }
+    
+    const convertStringToObject = (data) => {
+        /**
+         * convert stringify object to object
+         * If it's already an object than only return the data
+         */
+        return typeof data === 'string' ? JSON.parse(data) : data;
+    }
+    
+    const convertObjectToString = (data) => {
+        /**
+         * convert object to stringify object
+         * If it's already a string than only return the data
+         */
+        return typeof data === 'object' ? JSON.stringify(data) : data;
+    }
+    
+    
+    const storeTasksLocally = (task) => {
+        /**
+         * The work of this function is to get data from the localStorage
+         * than convert to object if it's not
+         * than convert to stringy object if it's not
+         * than save task data in the localStorage
+         */
+        let tasksData = getLocalTasksData('generalTasksData');
+    
+        tasksData = convertStringToObject(tasksData);
+        
+        let index = Object.keys(tasksData).length;
+        tasksData[index + 1] = task;
+        
+        let tasksDataStringify = convertObjectToString(tasksData);
+
+        setLocalTaskData('generalTasksData', tasksDataStringify);
+    }
+
+	return { getDatafromForm, storeTasksLocally};
 };
 
 const myData = Data();
